180fcdec2b2226e78ec30f22615bdb43bea2c414
B.N <19542357+BNSC1@users.noreply.github.com>
2022-07-25 09:18:20 +0800

    - Add: launch detail and launch list pages. - Delete: default theme, navigation and fragments setup

---
+++ app/src/main/java/com/bn/flights/ui/launch/LaunchDetailFragment.kt
@@ -0,0 +1,20 @@
+package com.bn.flights.ui.launch
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bn.flights.R
+import com.bn.flights.databinding.FragmentLaunchDetailBinding
+import com.bn.flights.ui.base.NavigationFragment
+
+class LaunchDetailFragment : NavigationFragment<FragmentLaunchDetailBinding>() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+
+        }
+    }
+}
