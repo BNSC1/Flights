2fad84925782555844f0150dfac2969c3cb5ecd4
B.N <19542357+BNSC1@users.noreply.github.com>
2022-07-26 09:30:08 +0800

    - Add: injection for main activity

--- app/src/main/java/com/bn/flights/ui/MainActivity.kt
+++ app/src/main/java/com/bn/flights/ui/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import com.bn.flights.R
 import com.bn.flights.databinding.ActivityMainBinding
 import com.bn.flights.ui.base.NavigationActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : NavigationActivity() {
     private lateinit var binding: ActivityMainBinding
     override val navHostId = R.id.fragment_container
